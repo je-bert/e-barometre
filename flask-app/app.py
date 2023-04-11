84500aa5db5fe62396dd51e13fd6af7bcffbcf5c
Jérémie Bertrand <jeremie.bertrand@hotmail.com>
2023-04-10 22:12:32 -0400

    Fix port

--- flask-app/app.py
+++ flask-app/app.py
@@ -28,4 +28,4 @@ def create_app():
     
 if __name__ == "__main__":
     from waitress import serve
-    serve(create_app(), host="0.0.0.0", port=8080)
+    serve(create_app(), host="0.0.0.0", port=5000)
