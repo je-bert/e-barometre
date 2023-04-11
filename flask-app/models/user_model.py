2035744a4f00d0fc44dc744ca0cbb3f7bf67a180
Jérémie Bertrand <jeremie.bertrand@hotmail.com>
2023-04-10 21:38:12 -0400

    update password length

--- flask-app/models/user_model.py
+++ flask-app/models/user_model.py
@@ -5,7 +5,7 @@ class User(db.Model):
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     email = db.Column(db.String(70), unique = True)
-    password = db.Column(db.String(80))
+    password = db.Column(db.String(255))
     phone_number = db.Column(db.String(20))
     date_logged_in = db.Column(db.DateTime())
     date_created = db.Column(db.DateTime())
