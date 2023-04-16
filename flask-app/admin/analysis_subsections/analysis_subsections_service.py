e2f46e91d55cf79228f03b5fca220b7c7672d2b1
Kui Hua Wang <98484628+KuiHua-JAC@users.noreply.github.com>
2023-04-16 07:58:37 -0400

    create analysis subsection add

--- flask-app/admin/analysis_subsections/analysis_subsections_service.py
+++ flask-app/admin/analysis_subsections/analysis_subsections_service.py
@@ -1,4 +1,4 @@
-from models import AnalysisSubsection
+from models import AnalysisSubsection,AnalysisSection
 from flask import render_template, abort, request, make_response, jsonify
 from database import db
 
@@ -32,3 +32,28 @@ def update_one(id):
   analysis_subsection.schema_type = data.get('schema_type') if data.get('schema_type') else None
   db.session.commit()
   return jsonify(analysis_subsection)
+
+def add_one(id):
+  if request.method == 'POST':
+      data = request.form
+
+      last_analysis_subsection = AnalysisSubsection.query.order_by(AnalysisSubsection.label_id.desc(),AnalysisSubsection.analysis_subsection_id.desc()).first()
+      if last_analysis_subsection:
+        analysis_subsection_id = int(last_analysis_subsection.analysis_subsection_id) + 1
+      else:
+        analysis_subsection_id = 1
+      while AnalysisSubsection.query.filter_by(analysis_subsection_id = analysis_subsection_id).first():
+        analysis_subsection_id += 1
+
+      analysis_subsection = AnalysisSubsection()
+      analysis_subsection.analysis_subsection_id = analysis_subsection_id
+      analysis_subsection.analysis_section_id = id
+      analysis_subsection.title = data.get('title')
+      analysis_subsection.description = data.get('description')
+      analysis_subsection.order = data.get('order') #TODO what should be the default value for order?
+      analysis_subsection.display_condition = data.get('display_condition') if data.get('display_condition') else None
+      analysis_subsection.schema_type = data.get('schema_type') if data.get('schema_type') else None
+      db.session.commit()
+      return jsonify(analysis_subsection)
+
+  return render_template('add-analysis-subsection.html')
