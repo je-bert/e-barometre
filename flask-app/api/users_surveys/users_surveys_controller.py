2074a5b8129ab245f6d1f0da751ec24d670def71
Simon <sleroux@carboniaweb.com>
2023-06-02 09:15:35 -0400

    typo

--- flask-app/api/users_surveys/users_surveys_controller.py
+++ flask-app/api/users_surveys/users_surveys_controller.py
@@ -8,4 +8,4 @@ users_surveys_router = Blueprint('users_surveys_router', __name__)
 @users_surveys_router.route('/<id>', methods=['POST'])
 @auth
 def create(current_user, id):
-    return users_surveys.create(current_user, id)
+    return users_surveys_service.create(current_user, id)
