73fddc154b7f1486ab427dd2b02068dea119e1f3
Simon <sleroux@carboniaweb.com>
2023-06-02 09:23:32 -0400

    wrong foreign key data type

--- flask-app/models/user_survey_model.py
+++ flask-app/models/user_survey_model.py
@@ -3,5 +3,5 @@ from database import db
 class User_Survey(db.Model):
     user_survey_id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
-    survey_id = db.Column(db.Integer, db.ForeignKey('survey.survey_id'))
+    survey_id = db.Column(db.String(255), db.ForeignKey('survey.survey_id'))
     is_complete = db.Column(db.Boolean())
