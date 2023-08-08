840bf26394d5d55db79fcbafa426fb3102f23281
Jérémie Bertrand <jeremie.bertrand@hotmail.com>
2023-08-08 18:29:44 -0400

    Hide create admin account for now (demo)

--- flask-app/admin/auth/auth_controller.py
+++ flask-app/admin/auth/auth_controller.py
@@ -7,9 +7,10 @@ auth_router = Blueprint('auth_router', __name__)
 def sign_in():
     return auth_service.sign_in()
 
-@auth_router.route('/sign-up', methods =['GET', 'POST'])
-def sign_up():
-    return auth_service.sign_up()
+# hide-demo
+# @auth_router.route('/sign-up', methods =['GET', 'POST'])
+# def sign_up():
+#     return auth_service.sign_up()
 
 @auth_router.route('/sign-out')
 def sign_out():
