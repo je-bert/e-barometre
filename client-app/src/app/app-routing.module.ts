0e1f2c7650f2bab5e9b3ea72c8aeaa60cddd6968
MathieuLussier <mathieu.lussier@hotmail.com>
2023-09-29 12:56:11 -0400

    enable initial navigation

--- client-app/src/app/app-routing.module.ts
+++ client-app/src/app/app-routing.module.ts
@@ -121,7 +121,10 @@ const routes: Routes = [
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+    RouterModule.forRoot(routes, { 
+    scrollPositionRestoration: 'enabled',
+    initialNavigation: 'enabledBlocking',
+  }),
   ],
   exports: [RouterModule],
 })
