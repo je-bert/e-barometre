2bfad576aebb8a1857c6d651dc50cf49c19ba1fb
Jérémie Bertrand <jeremie.bertrand@hotmail.com>
2023-09-29 13:46:11 -0400

    fix check password

--- flask-app/utils/check_password.py
+++ flask-app/utils/check_password.py
@@ -1,6 +1,6 @@
 import re
 
-regex = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+regex = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%&*+?])[A-Za-z\d!@#$%&*+?]{8,}$"
 
 # Minimum eight characters, at least one letter and one number:
 
