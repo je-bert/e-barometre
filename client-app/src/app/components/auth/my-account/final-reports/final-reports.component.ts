5af70adf5fbe7f8fee3d73ab65d05917040e436b
Kui Hua Wang <98484628+KuiHua-JAC@users.noreply.github.com>
2023-10-02 14:10:53 -0400

    add final report checkout

--- client-app/src/app/components/auth/my-account/final-reports/final-reports.component.ts
+++ client-app/src/app/components/auth/my-account/final-reports/final-reports.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Account } from 'src/app/models/account';
+import { AccountService } from 'src/app/services/account.service';
+import { FinalReportService } from 'src/app/services/final_reports.service';
 
+declare var Stripe: any;
 @Component({
   selector: 'app-final-reports',
   templateUrl: './final-reports.component.html',
-  styleUrls: ['./final-reports.component.scss']
+  styleUrls: ['./final-reports.component.scss'],
+  providers: [FinalReportService, AccountService],
 })
 export class FinalReportsComponent implements OnInit {
+  constructor(
+    private finalReportService: FinalReportService,
+    private accountService: AccountService
+  ) {}
 
-  constructor() { }
+  public publishableKey: String | undefined;
+  public userEmail: String;
+  private sessionId: String | undefined;
 
-  ngOnInit(): void {
+  public redirectStripeCheckout(reportType: String): void {
+    this.finalReportService.getPublishableKey().subscribe((key) => {
+      this.publishableKey = key.publishable_key;
+      const stripe = Stripe(this.publishableKey);
+      this.finalReportService
+        .getSessionId(reportType, this.userEmail)
+        .subscribe((sessionId) => {
+          this.sessionId = sessionId.session_id;
+          console.log(this.sessionId);
+        });
+      return stripe.redirectToCheckout({ sessionId: this.sessionId });
+    });
   }
 
+  ngOnInit(): void {
+    this.accountService.getAccount().subscribe((account: Account) => {
+      this.userEmail = account.email;
+    });
+  }
 }
---
+++ client-app/src/app/services/final_reports.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class FinalReportService {
+  constructor(private http: HttpClient, private router: Router) {}
+
+  public getPublishableKey(): Observable<any> {
+    return this.http.get<any>(environment.apiUrl + '/stripe/publishable-key');
+  }
+
+  public getSessionId(reportType: String, email: String): Observable<any> {
+    return this.http.post(
+      environment.apiUrl + '/stripe/create-checkout-session',
+      { productId: reportType, email: email }
+    );
+  }
+}
