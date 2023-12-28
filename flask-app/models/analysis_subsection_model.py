90cc36b86b81273e1c142295287d3423725a3a24
Jérémie Bertrand <jeremie.bertrand@hotmail.com>
2023-12-27 20:56:07 -0500

    update seed

--- flask-app/models/analysis_subsection_model.py
+++ flask-app/models/analysis_subsection_model.py
@@ -7,16 +7,18 @@ class AnalysisSubsection(db.Model):
     analysis_section_id: str
     title: str
     about_barometer: str
-    flag_introduction: str
     order: int 
     display_condition: str
     barometer: str
+    min_result: float
+    min_weight: float
 
     analysis_subsection_id = db.Column(db.String(20), primary_key = True)
     analysis_section_id = db.Column(db.String(20), db.ForeignKey( "analysis_section.analysis_section_id"))
     title = db.Column(db.String(255))
     about_barometer = db.Column(db.String(255))
-    flag_introduction = db.Column(db.String(255))
     order = db.Column(db.Integer)
     display_condition = db.Column(db.String(255), nullable = True)
     barometer = db.Column(db.String(255))
+    min_result = db.Column(db.Float)
+    min_weight = db.Column(db.Float)
